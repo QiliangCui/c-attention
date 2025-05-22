239748efa00c71c7d8d30813d695798264be4642
Qiliang Cui <derrhein@gmail.com>
2025-05-22 23:48:53 +0000

    failed the test.

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -5,7 +5,7 @@ def test_softmax_1d():
     x = np.array([1.0, 2.0, 3.0])
     y = softmax(x)
     assert np.allclose(np.sum(y), 1.0)
-    assert y.shape == x.shape
+    assert y.shape != x.shape
 
 def test_softmax_2d():
     x = np.array([[1.0, 2.0, 3.0], [1.0, 2.0, 3.0]])
