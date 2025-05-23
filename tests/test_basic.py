d761279f058f66953d46ffbd1ebaec46791e80e3
Qiliang Cui <derrhein@gmail.com>
2025-05-23 14:49:04 +0000

    fix test.

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -5,10 +5,10 @@ def test_softmax_1d():
     x = np.array([1.0, 2.0, 3.0])
     y = softmax(x)
     assert np.allclose(np.sum(y), 1.0)
-    assert y.shape != x.shape
+    assert y.shape == x.shape
 
 def test_softmax_2d():
     x = np.array([[1.0, 2.0, 3.0], [1.0, 2.0, 3.0]])
     y = softmax(x, axis=1)
     assert np.allclose(np.sum(y, axis=1), np.ones(x.shape[0]))
-    assert y.shape != x.shape
+    assert y.shape == x.shape
